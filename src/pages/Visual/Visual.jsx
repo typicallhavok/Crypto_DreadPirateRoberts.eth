e4c279ea3dc570b24e50a8d0b6fe6ff76e72f808
KPrabhu711 <kp711work@gmail.com>
2024-03-02 16:51:07 +0530

    Update Visual.jsx

--- src/pages/Visual/Visual.jsx
+++ src/pages/Visual/Visual.jsx
@@ -4,13 +4,30 @@ import "./Visual.css";
 
 const Visual = () => {
     const location = useLocation();
-    const x=location.state.txData;
-    console.log(x)
+    const txData=location.state.txData;
+    console.log(txData)
+    const inputs = txData.inputs.map(input => input.prev_out.addr);
+    const outputs = txData.out.map(output => output.addr);
+
+    // Calculate total amount sent
+    const totalAmountSent = txData.out.reduce((total, output) => {
+        return total + output.value;
+    }, 0) / 100000000; // Convert satoshi to BTC
+
+    // Log transaction details
+    console.log("Sender Address:", inputs);
+    console.log("Receiver Address:", outputs);
+    console.log("Amount Sent (BTC):", totalAmountSent);
+    console.log("Transaction Data:", txData);
     return(
         <>
-        {x.hash}
+        {txData.hash}
         </>
     )
 }
+
+
+
+
 
 export default Visual;
