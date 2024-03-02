b1a5d46a569cd8e215440b8213f78bc121a93fa4
Amogh <diamondpineappleboy@gmail.com>
2024-03-02 23:24:30 +0530

    css

--- src/pages/Visual/Visual.jsx
+++ src/pages/Visual/Visual.jsx
@@ -12,28 +12,32 @@ const Visual = () => {
     const inputs = txData.inputs.map(input => input.prev_out.addr);
     const outputs = txData.out.map(output => output.addr);
     const redirectToVisual = () => {
-        window.location.href=`https://oxt.me/transaction/${txData.hash}`;
+        window.open(`https://oxt.me/transaction/${txData.hash}`);
     };
     const totalAmountSent = txData.out.reduce((total, output) => {
         return total + output.value;
     }, 0) / 100000000; 
 
-    console.log("Sender Address:", inputs);
-    console.log("Receiver Address:", outputs);
     console.log("Amount Sent (BTC):", totalAmountSent);
     console.log("Transaction Data:", txData);
+    console.log(inputs.length);
     return(
         <>
         <h1>Transactions</h1>
         <div className="container1">
-            <button onClick={redirectToVisual}>View transaction flow</button>
+            <div className="track">
+                {(totalAmountSent>16||inputs.length>30||outputs.length>30) && <div className="sus">This transaction is suspicious</div>}
+                <button onClick={redirectToVisual}>View transaction flow</button>
+            </div>
             <div className="txDets">
                 <div className="txInputs">
+                    <h2>Input Addreses</h2>
                     {inputs.map((input, index) => (
                     <span onClick="" className="eachTx" key={index}>{input}</span>
                     ))}
                 </div>
                 <div className="txOutputs">
+                <h2>Output Addreses</h2>
                     {outputs.map((output, index) => (
                     <span className="eachTx" key={index}>{output}</span>
                     ))}
